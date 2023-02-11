22ecd0239d2e64266c24f9736d68b1fe84dfc694
kotoe <121812120+koto1996@users.noreply.github.com>
2023-02-11 15:26:45 +0000

    アプリケーション２課題３Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+ devise_for :users, controllers: {
+   sessions: 'users/sessions',
+   registrations: 'users/registrations',
+ }
 
   root to:"homes#top"
   get 'homes/about'=>'homes#about',as:'about'
-  devise_for :users
   resources :books,only:[:new,:index,:show,:edit]
   resources :users,only:[:index,:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
