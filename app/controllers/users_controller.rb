cb9a7dfe5b18c4e08ae2e024332e2eca942b6252
kotoe <121812120+koto1996@users.noreply.github.com>
2023-02-13 07:15:30 +0000

    アプリケーション２課題３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
+   def new
+   @book=Book.new
+  end
+  
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to book_path(@book.user_id)
+  end
+  
   def index
+    @users=User.all
   end
 
   def show
     @user=User.find(params[:id])
     @books=@user.books
     @book=Book.new
-  end
-  
-  def create
-    @book=Book.new
-    @book.user_id=current_user.id
-    @book.save
-    redirect_to book_path(@book.user_id)
   end
 
   def edit
