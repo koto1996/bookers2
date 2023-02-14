071ec3d8e83cf5a4e6cd041a4a9405b27cb527f1
kotoe <121812120+koto1996@users.noreply.github.com>
2023-02-14 04:57:48 +0000

    アプリケーション２課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,17 @@ class BooksController < ApplicationController
   
 
   def index
+   @book=Book.new
    @books=Book.all
   end
+  
+  def create
+   @book=Book.new(book_params[:id])
+   @book.user_id=current_user.id
+   @book.save
+   redirect_to book_path(@book.user_id)
+  end
+  
 
   def show
    @book=Book.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     @book=Book.new
     @users=User.all
-    @user=User.find(params[:id])
+    @user=current_user
   end
 
   def show
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
     @books=@user.books
     @book=Book.new
   end
-  
+
   def create
-    @book=Book.new(book_params)
+    @book=Book.new
     @book.user_id=current_user.id
     @book.save
     redirect_to book_path(@book.user_id)
@@ -22,20 +22,17 @@ class UsersController < ApplicationController
   def edit
    @user=User.find(params[:id])
   end
-  
+
   def update
    @user=User.find(params[:id])
    @user.update(user_params)
    redirect_to user_path(@user.id)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
-  
-  def book_params
-   params.require(:book).permit(:title,:body)
-  end
+
 end
